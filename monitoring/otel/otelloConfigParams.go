116fd3f6e77737367a40d22c3ff48be2c88e68c7
Snake <snakerius@gmail.com>
2026-09-24 12:46:14 +0700

    defaults for otello 0.1.7

--- monitoring/otel/otelloConfigParams.go
+++ monitoring/otel/otelloConfigParams.go
@@ -1,12 +1,12 @@
 package otel
 
 type ConfigOtello struct {
-	Url               string            `yaml:"url"            env:"OTELLO_URL"`
-	ServiceName       string            `yaml:"serviceName"    env:"OTELLO_SERVICE_NAME"`
-	UnqServiceName    string            `yaml:"unqServiceName" env:"OTELLO_UNQ_SERVICE_NAME"`
-	Labels            map[string]string `yaml:"labels"         env:"OTELLO_LABELS"`
-	Debug             bool              `yaml:"debug"          env:"OTELLO_DEBUG"` // Deprecated: reserved, not used.
+	Url               string            `yaml:"url"            env:"OTELLO_URL"               default:"localhost:4317"`
+	ServiceName       string            `yaml:"serviceName"    env:"OTELLO_SERVICE_NAME"                              `
+	UnqServiceName    string            `yaml:"unqServiceName" env:"OTELLO_UNQ_SERVICE_NAME"                          `
+	Labels            map[string]string `yaml:"labels"         env:"OTELLO_LABELS"                                    `
+	Debug             bool              `yaml:"debug"          env:"OTELLO_DEBUG"                                     ` // Deprecated: reserved, not used.
 	WithoutPrometheus bool
-	SampleRatio       float64 `yaml:"sampleRatio" env:"OTELLO_SAMPLE_RATIO" default:"1.0"`
-	TLS               bool    `yaml:"tls"         env:"OTELLO_TLS"`
+	SampleRatio       float64 `yaml:"sampleRatio" env:"OTELLO_SAMPLE_RATIO" default:"1.0"                             `
+	TLS               bool    `yaml:"tls"         env:"OTELLO_TLS"                                                    `
 }
